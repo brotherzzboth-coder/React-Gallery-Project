75070c77058f9b9290566b0509d0b267cd16401c
Krishna Gupta <brotherzzboth@gmail.com>
2026-02-06 14:55:43 +0530

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: '/Gallery-App/',
+  plugins: [react(), tailwindcss()],
 })
